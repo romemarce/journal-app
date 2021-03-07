434e5ce84bbaecfe66704d3e6dd3fba15681e919
Marcelo <romemarce1@gmail.com>
2021-03-07 18:41:50 -0300

    add useForm, auth actions, and update Login screen function

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,23 +1,47 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { login } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
+
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({
+    email: "romerodev001@gmail.com",
+    password: "123456",
+  });
+
+  const {email, password} = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(
+      login(12345, 'Marcelo')
+    )
+  }
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           className="auth__input"
           type="text"
           placeholder="Email"
           name="email"
           autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
           type="password"
           placeholder="Password"
           name="password"
+          value={password}
+          onChange={handleInputChange}
         />
         <button className="btn-block btn btn-primary" type="submit">
           Login
